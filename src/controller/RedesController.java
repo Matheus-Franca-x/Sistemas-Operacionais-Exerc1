f04f35057e3bf2742edba0b793c07baef17b6500
matheus <matheussss6613@gmail.com>
2023-02-28 10:09:56 -0300

    Criação de funções callProcess e readprocess

--- src/controller/RedesController.java
+++ src/controller/RedesController.java
@@ -1,5 +1,9 @@
 package controller;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class RedesController {
 	
 	public RedesController() 
@@ -7,13 +11,21 @@ public class RedesController {
 		super();
 	}
 	
-	public String callSO()
+	private String OS()
 	{
 		String os = System.getProperty("os.name");
-		return os;
+		String arch = System.getProperty("os.arch");
+		String version = System.getProperty("os.version");
+		
+		return os + " - v. " + version + " - arch. " + arch;
 	}
 	
-	public void callIp()
+	public String callOS()
+	{
+		return OS();
+	}
+	
+	public void IP()
 	{
 		
 	}
@@ -23,4 +35,52 @@ public class RedesController {
 		
 	}
 	
+	public void callProcess(String process) 
+	{
+		try
+		{
+			Runtime.getRuntime().exec(process);
+		} catch(Exception e) {
+			String msgErro = e.getMessage();
+			if(msgErro.contains("740"))
+			{
+				StringBuffer buffer = new StringBuffer();
+				buffer.append("cmd /c");
+				buffer.append(" ");
+				buffer.append(process);
+				try {
+					Runtime.getRuntime().exec(buffer.toString());
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+			else 
+			{
+				System.err.println(msgErro);
+			}
+		}
+	}
+	
+	public void readProcess(String process)
+	{
+		try {
+			Process p = Runtime.getRuntime().exec(process);
+			InputStream fluxo = p.getInputStream();
+			InputStreamReader leitor = new InputStreamReader(fluxo);
+			BufferedReader buffer = new BufferedReader(leitor);
+			String linha = buffer.readLine();
+			while (linha != null)
+			{
+				System.out.println(linha);
+				linha = buffer.readLine();
+			}
+			buffer.close();
+			leitor.close();
+			fluxo.close();
+			
+			} catch (Exception e) {
+			String msgErro = e.getMessage();
+			System.out.println(msgErro);
+		}
+	}
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -7,9 +7,13 @@ public class Main
 	public static void main(String[] args)
 	{
 		RedesController redeControll = new RedesController();
-		String os = redeControll.callSO();
+		String os = redeControll.callOS();
 		
 		System.out.println(os);
 		
+
+		
+//		String process = "ipconfig";
+//		redeControll.readProcess(process);
 	}
 }
